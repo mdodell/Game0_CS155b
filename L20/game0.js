3359dc538abf772c7ab681f7cfa72bc4c5ef54e4
Devi Acharya <dacharya64@gmail.com>
2018-03-21 00:41:13 -0400

    npc now faces player
     moves towards them when within certain distance and added extra feature (player flip on pressing u)

--- L20/game0.js
+++ L20/game0.js
@@ -6,26 +6,25 @@ The user moves a cube around the board trying to knock balls into a cone
 
 */
 
-
 	// First we declare the variables that hold the objects we need
 	// in the animation code
 	var scene, renderer;  // all threejs programs need these
-	var camera, avatarCam;  // we have two cameras in the main scene
-	var avatar;
+	var camera, avatarCam, edgeCam;  // we have two cameras in the main scene
+	var avatar, npc;
 	// here are some mesh objects ...
 
 	var cone;
 
 	var introScene, introCamera, introText;
 	var endScene, endCamera, endText;
+	var loseScene, loseCamera, loseText;
 
 
 	// create an AudioListener and add it to the camera
-var listener1 = new THREE.AudioListener();
+	var listener1 = new THREE.AudioListener();
 
-// create the PositionalAudio object (passing in the listener)
-var sound1 = new THREE.PositionalAudio( listener1 );
-
+	// create the PositionalAudio object (passing in the listener)
+	var sound1 = new THREE.PositionalAudio( listener1 );
 
 	var controls =
 	     {fwd:false, bwd:false, left:false, right:false,
@@ -40,7 +39,7 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 
 
 	// Here is the main game control
-  init(); //
+	init(); //
 	initControls();
 	animate();  // start the animation loop!
 
@@ -74,6 +73,18 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 		endCamera.lookAt(0,0,0);
 
 	}
+	function createLoseScene(){
+		loseScene = initScene();
+		loseText = createSkyBox('youlose.png',10);
+
+		loseScene.add(loseText);
+		var light1 = createPointLight();
+		light1.position.set(0,200,20);
+		loseScene.add(light1);
+		loseCamera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
+		loseCamera.position.set(0,50,1);
+		loseCamera.lookAt(0,0,0);
+	}
 
 	/**
 	  To initialize the scene, we initialize each of its components
@@ -82,6 +93,7 @@ var sound1 = new THREE.PositionalAudio( listener1 );
       initPhysijs();
 			scene = createIntroScene();
 			createEndScene();
+			createLoseScene();
 			initRenderer();
 	}
 
@@ -101,8 +113,6 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 			camera.position.set(0,50,0);
 			camera.lookAt(0,0,0);
 
-
-
 			// create the ground and the skybox
 			var ground = createGround('grass.png');
 			scene.add(ground);
@@ -111,14 +121,25 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 
 			// create the avatar
 			avatarCam = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
-			avatar = createAvatar();
-			avatar.translateY(20);
-			avatarCam.translateY(-4);
+			createAvatar();
+
+			avatarCam.translateY(4);
 			avatarCam.translateZ(3);
-			scene.add(avatar);
+			avatarCam.rotation.y = Math.PI;
+
 			gameState.camera = avatarCam;
 
+			edgeCam = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
 			addBalls();
+
+			npc = createBoxMesh(0x0000ff);
+			npc.position.set(30,5,-30);
+			npc.scale.set(1,2,4);
+			npc.rotateX(Math.PI / 2);
+			scene.add(npc);
+			console.dir(npc);
 
 			cone = createConeMesh(4,6);
 			cone.position.set(10,3,7);
@@ -143,9 +164,6 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 		return Math.random()*n;
 	}
 
-
-
-
 	function addBalls(){
 		var numBalls = 2
 
@@ -155,7 +173,7 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 			ball.position.set(randN(20)+15,30,randN(20)+15);
 			scene.add(ball);
 
-			ball.addEventListener( 'collision',
+			ball.addEventListener('collision',
 				function( other_object, relative_velocity, relative_rotation, contact_normal ) {
 					if (other_object==cone){
 						console.log("ball "+i+" hit the cone");
@@ -173,8 +191,6 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 			)
 		}
 	}
-
-
 
 	function playGameMusic(){
 		// create an AudioListener and add it to the camera
@@ -194,7 +210,6 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 		});
 	}
 
-
 	function soundEffect(file){
 		// create an AudioListener and add it to the camera
 		var listener = new THREE.AudioListener();
@@ -213,18 +228,19 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 		});
 	}
 
-	/* We don't do much here, but we could do more!
-	*/
+	/* We don't do much here, but we could do more!*/
 	function initScene(){
 		//scene = new THREE.Scene();
     var scene = new Physijs.Scene();
 		return scene;
 	}
 
-  function initPhysijs(){
-    Physijs.scripts.worker = '/js/physijs_worker.js';
-    Physijs.scripts.ammo = '/js/ammo.js';
-  }
+	function initPhysijs(){
+
+		Physijs.scripts.worker = '/js/physijs_worker.js';
+		Physijs.scripts.ammo = '/js/ammo.js';
+	}
+
 	/*
 		The renderer needs a size and the actual canvas we draw on
 		needs to be added to the body of the webpage. We also specify
@@ -261,6 +277,7 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 		mesh.castShadow = true;
 		return mesh;
 	}
+
 	function createImageMesh(image){
 		var geometry = new THREE.BoxGeometry( 1, 1, 1);
 		var texture = new THREE.TextureLoader().load( '../images/'+image );
@@ -273,7 +290,6 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 		mesh.castShadow = true;
 		return mesh;
 	}
-
 
 	function createGround(image){
 		// creating a textured plane which receives shadows
@@ -294,8 +310,6 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 		// we need to rotate the mesh 90 degrees to make it horizontal not vertical
 	}
 
-
-
 	function createSkyBox(image,k){
 		// creating a textured plane which receives shadows
 		var geometry = new THREE.SphereGeometry( 80, 80, 80 );
@@ -310,37 +324,56 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 
 		mesh.receiveShadow = false;
 
-
-		return mesh
 		// we need to rotate the mesh 90 degrees to make it horizontal not vertical
-
-
+		return mesh
 	}
 
 	function createAvatar(){
-		//var geometry = new THREE.SphereGeometry( 4, 20, 20);
-		var geometry = new THREE.BoxGeometry( 5, 5, 6);
-		var material = new THREE.MeshLambertMaterial( { color: 0xffff00} );
-		var pmaterial = new Physijs.createMaterial(material,0.9,0.5);
-		//var mesh = new THREE.Mesh( geometry, material );
-		var mesh = new Physijs.BoxMesh( geometry, pmaterial );
-		mesh.setDamping(0.1,0.1);
-		mesh.castShadow = true;
 
-		avatarCam.position.set(0,4,0);
-		avatarCam.lookAt(0,4,10);
-		mesh.add(avatarCam);
-		avatarCam.add(listener1);
+		// Constructs the OBJ Loader
+		var loader = new THREE.JSONLoader();
+		loader.load(
 
+		// File Location
+		"/models/suzanne.json",
 
+			// Function called upon load
+			function (geometry, materials){
 
-		return mesh;
+				var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+				var pmaterial = new Physijs.createMaterial(material,0.9,0.5);
+				monkey = new Physijs.BoxMesh( geometry, pmaterial );
+
+				monkey.setDamping(0.1,0.1);
+				monkey.castShadow = true;
+
+				// Sets the avatar to the mesh
+				avatar = monkey;
+				avatar.add(avatarCam);
+				avatar.translateY(20);
+				scene.add(avatar);
+				avatar.addEventListener('collision',
+					function( other_object, relative_velocity, relative_rotation, contact_normal ) {
+						if (other_object==npc){
+							console.log("npc "+i+" hit the avatar");
+							gameState.health -= 1;  // add one to the score
+							avatar.position.set(randN(20)+15,avatar.position.y,randN(20)+15);
+							avatar.__dirtyPosition = true;
+							if (gameState.health<1) {
+								gameState.scene='youlose';
+							}
+						}
+					}
+				)
+				console.log("done");
+
+			}
+			);
+
 	}
 
 
-
-
-// finally add the sound to the mesh
+	// finally add the sound to the mesh
 	function createConeMesh(r,h){
 		var geometry = new THREE.ConeGeometry( r, h, 32);
 		var texture = new THREE.TextureLoader().load( '../images/tile.jpg' );
@@ -352,14 +385,8 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 		var mesh = new Physijs.ConeMesh( geometry, pmaterial, 0 );
 		mesh.add( sound1 ); //adds Jaws music to cone as you get closer
 		mesh.castShadow = true;
-		this.__dirtyPosition = true;
-		this.__dirtyRotation = true;
 		return mesh;
 	}
-
-
-
-
 
 	function createBall(){
 		//var geometry = new THREE.SphereGeometry( 4, 20, 20);
@@ -371,10 +398,6 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 		mesh.castShadow = true;
 		return mesh;
 	}
-
-
-
-
 
 	var clock;
 
@@ -396,6 +419,14 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 		if (gameState.scene == 'youwon' && event.key=='r') {
 			gameState.scene = 'main';
 			gameState.score = 0;
+			gameState.health = 10;
+			addBalls();
+			return;
+		}
+		if(gameState.scene == 'youlose' && event.key=='r'){
+			gameState.scene = 'main';
+			gameState.score = 0;
+			gameState.health = 10;
 			addBalls();
 			return;
 		}
@@ -410,15 +441,20 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 			case "r": controls.up = true; break;
 			case "f": controls.down = true; break;
 			case "m": controls.speed = 30; break;
-      case " ": controls.fly = true; break;
+      case " ":
+				console.log("I'm flying!");
+				controls.fly = true;
+				break;
       case "h": controls.reset = true; break;
+			case "~": gameState.scene = 'youlose'; break;
+			case "u": controls.flipPlayer = true; break;
 
 			case "p": scene = initScene(); createMainScene(); break;
-
 
 			// switch cameras
 			case "1": gameState.camera = camera; break;
 			case "2": gameState.camera = avatarCam; break;
+			case "3": gameState.camera = edgeCam; break;
 
 			//rotate avatar camera
 			case "q": controls.leftCamera = true; break;
@@ -445,18 +481,16 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 			case "r": controls.up    = false; break;
 			case "f": controls.down  = false; break;
 			case "m": controls.speed = 10; break;
-      case " ": controls.fly = false; break;
-      case "h": controls.reset = false; break;
+		    case " ": controls.fly = false; break;
+		    case "h": controls.reset = false; break;
+				case "~": gameState.scene = 'youlose'; break;
 
 			//rotate avatar camera
 			case "q": controls.leftCamera = false; break;
 			case "e": controls.rightCamera = false;break;
+			case "u": controls.flipPlayer = false; break;
 		}
 	}
-
-
-
-
 
   function updateAvatar(){
 		"change the avatar's linear or angular velocity based on controls state (set by WSAD key presses)"
@@ -465,17 +499,19 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 
 		if (controls.fwd){
 			avatar.setLinearVelocity(forward.multiplyScalar(controls.speed));
-		} else if (controls.bwd){
+		}
+		else if (controls.bwd){
 			avatar.setLinearVelocity(forward.multiplyScalar(-controls.speed));
-		} else {
+		}
+		else {
 			var velocity = avatar.getLinearVelocity();
 			velocity.x=velocity.z=0;
 			avatar.setLinearVelocity(velocity); //stop the xz motion
 		}
 
-    if (controls.fly){
-      avatar.setLinearVelocity(new THREE.Vector3(0,controls.speed,0));
-    }
+		if (controls.fly){
+		  avatar.setLinearVelocity(new THREE.Vector3(0,controls.speed,0));
+		}
 
 		if (controls.left){
 			avatar.setAngularVelocity(new THREE.Vector3(0,controls.speed*0.1,0));
@@ -483,10 +519,10 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 			avatar.setAngularVelocity(new THREE.Vector3(0,-controls.speed*0.1,0));
 		}
 
-    if (controls.reset){
-      avatar.__dirtyPosition = true;
-      avatar.position.set(40,10,40);
-    }
+		if (controls.reset){
+		  avatar.__dirtyPosition = true;
+		  avatar.position.set(40,10,40);
+		}
 
 		if (controls.leftCamera){
 			avatarCam.rotateY(0.01);
@@ -494,22 +530,27 @@ var sound1 = new THREE.PositionalAudio( listener1 );
 			avatarCam.rotateY(-0.01);
 		}
 
+		if (controls.flipPlayer){
+			avatar.__dirtyRotation = true;
+			avatar.rotateX(Math.PI/2);
+		}
 	}
 
 
-function updateEnemy() {
-	//var forward = cone.getWorldDirection();
-	//cone.setLinearVelocity(new THREE.Vector3(0,0,10));
-	cone.lookAt(avatar.position);
-	cone.translateZ(.1);
-}
 
+	function updateNPC(){
+
+			npc.lookAt(avatar.position);
+		  npc.__dirtyPosition = true;
+			npc.__dirtyRotation = true;
+			if (npc.position.distanceTo(avatar.position) < 25) {
+				npc.setLinearVelocity(npc.getWorldDirection().multiplyScalar(3));
+			}
+	}
 
 	function animate() {
 
 		requestAnimationFrame( animate );
-
-
 
 		switch(gameState.scene) {
 
@@ -525,21 +566,24 @@ function updateEnemy() {
 
 			case "main":
 				updateAvatar();
-				updateEnemy();
+				updateNPC();
 	    	scene.simulate();
+				edgeCam.lookAt(avatar.position)
 				if (gameState.camera!= 'none'){
 					renderer.render( scene, gameState.camera );
 				}
 				var info = document.getElementById("info");
 				elapsedTime = (new Date().getTime() - startDate.getTime()) / 1000;
-			 	info.innerHTML='<div style="font-size:24pt">Score: ' + gameState.score + '&nbsp;&nbsp;Time Taken: ' + parseInt(elapsedTime) + ' seconds&nbsp;&nbsp;&lt;/&gt; with &hearts; by Team 10</div>';
-
+			 	info.innerHTML='<div style="font-size:24pt">Score: ' + gameState.score + '&nbsp;&nbsp;Health: ' + gameState.health + '&nbsp;&nbsp;Time Taken: ' + parseInt(elapsedTime) + ' seconds&nbsp;&nbsp;&lt;/&gt; with &hearts; by Team 10</div>';
 				break;
+
+				case "youlose":
+				loseText.rotateY(0.005);
+				renderer.render(loseScene,loseCamera);
+				break;
+
 
 			default:
 			  console.log("don't know the scene "+gameState.scene);
-
 		}
-
-
 	}
